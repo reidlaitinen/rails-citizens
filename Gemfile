6d1c4851705067d3bbcf4fe3c6c3f78cfa4f7251
Reid Laitinen <reidlaitinen@gmail.com>
2018-02-27 19:23:03 -0700

    now seeding with faker

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ end
 
 ruby '2.5.0'
 
+gem 'faker'
+gem 'colorize'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
 # Use postgresql as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do |i|
+  first_name = Faker::GameOfThrones.character.split(' ')[0]
+  last_name = Faker::GameOfThrones.character.split(' ')[1]
+  age = rand(14..65)
+  hair_color = Faker::Color.color_name
+  eye_color = Faker::Color.color_name
+  gender = ['Male','Female'].sample
+  alive = [true, false].sample
+  Person.create(first_name: first_name, last_name: last_name, age: age, hair_color: hair_color, eye_color: eye_color, gender: gender, alive: alive)
+end
